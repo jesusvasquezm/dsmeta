16542528cb93d090a3814daf69d3d3179d418844
Jesus Vasquez M <jesusvasquezm098@gmail.com>
2022-09-06 11:36:37 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,11 @@
-import logo from '../../assets/img/logo.svg'
-import './style.css'
+import logo from '../../assets/img/logo.svg';
+import './style.css';
 function Header() {
   return (
     <header>
       <div className="dsmeta-logo-container">
         <img src={logo} alt="DSMeta" />
         <h1>DSMeta</h1>
-        <p>
-          Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
-        </p>
       </div>
     </header>
   );
